6fd9aa7fd41f721151e7538bb8659aee80b90708
Atul Sharma <atulsharma20may@gmail.com>
2021-06-05 13:19:22 +0530

    Fix: padding in card Update: UI

--- user-app/src/main/java/com/streamliners/myecom/MainActivity.java
+++ user-app/src/main/java/com/streamliners/myecom/MainActivity.java
@@ -3,12 +3,40 @@ package com.streamliners.myecom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.streamliners.myecom.databinding.ActivityMainBinding;
+import com.streamliners.myecom.databinding.ItemVbProductBinding;
+import com.streamliners.myecom.databinding.ItemWbProductBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+
+        for (int i = 1; i <= 10; i++) {
+                ItemWbProductBinding wbCardBinding = ItemWbProductBinding.inflate(getLayoutInflater());
+
+                ItemVbProductBinding vbCardBinding = ItemVbProductBinding.inflate(getLayoutInflater());
+                vbCardBinding.btnVariants.setOnClickListener(view -> {
+                    if (vbCardBinding.btnVariants.getRotation() == 180) {
+                        vbCardBinding.btnVariants.setRotation(0);
+                        vbCardBinding.variants.setVisibility(View.GONE);
+                        vbCardBinding.cl.setPadding(0, 0, 0, 56);
+                        return;
+                    }
+                    vbCardBinding.btnVariants.setRotation(180);
+                    vbCardBinding.variants.setVisibility(View.VISIBLE);
+                    vbCardBinding.cl.setPadding(0, 0, 0, 8);
+                });
+
+                mainBinding.list.addView(vbCardBinding.getRoot());
+                mainBinding.list.addView(wbCardBinding.getRoot());
+        }
     }
 }
